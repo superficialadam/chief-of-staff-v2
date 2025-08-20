52bcaa9618b4919eb99bd48281aab992586ef7e1
Adam Wittsell <adam@superficialmachines.com>
2025-08-20 14:56:53 +0200

    Jobs work

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,10 @@ module ChiefOfStaffV2
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
+    #
+    # Use Solid Queue for Active Job everywhere
 
+    config.active_job.queue_adapter = :solid_queue
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
